0107bad52a4459f6077d30d48b0c5d7c7974d265
NickDienemann <nick@eckel-dienemann.de>
2021-09-15 09:37:26 +0200

    updated gitignore

---
+++ powershell_scripts/pre_setup.ps1
@@ -0,0 +1,4 @@
+cd 'C:\Users\nick\Code\Python_Projects\bachelorarbeit_tom'
+
+Copy-Item -Path "C:\Users\nick\Code\Python_Projects\bachelorarbeit_tom\web_app\templates" -Destination "C:\Users\nick\Code\Python_Projects\bachelorarbeit_tom" -Recurse
+Copy-Item -Path "C:\Users\nick\Code\Python_Projects\bachelorarbeit_tom\web_app\static" -Destination "C:\Users\nick\Code\Python_Projects\bachelorarbeit_tom" -Recurse
